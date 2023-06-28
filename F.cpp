4a9dca076fdf2f9e2aae353cbc27105dd6cbe4fd
GithubHakiki <127917441+GithubHakiki@users.noreply.github.com>
2023-06-29 02:28:23 +0700

    F.cpp

--- F.cpp
+++ F.cpp
@@ -5,22 +5,22 @@
 
 using namespace std;
 
-vector<string> getInput()
-{
-	vector<string> data;
-	string input;
-
-	cout << "masukkan teks:\n";
-
-	while(true){
-		getline(cin, input);
-		if(input.empty()){
-			break;
-		}
-		data.push_back(input);
-	}
-	return data;
-}
+//vector<string> getInput()
+//{
+//	vector<string> data;
+//	string input;
+//
+//	cout << "masukkan teks:\n";
+//
+//	while(true){
+//		getline(cin, input);
+//		if(input.empty()){
+//			break;
+//		}
+//		data.push_back(input);
+//	}
+//	return data;
+//}
 
 int main()
 {
@@ -46,12 +46,12 @@ int main()
 	
 	is.close();
 	
-	vector<string> userInput = getInput();
-	cout << "data yang dimasukkan:\n";
-	for (const string& data: userInput)
-	{
-		cout << data << endl;
-	}
+//	vector<string> userInput = getInput();
+//	cout << "data yang dimasukkan:\n";
+//	for (const string& data: userInput)
+//	{
+//		cout << data << endl;
+//	}
 	
 	return 0;
 }
